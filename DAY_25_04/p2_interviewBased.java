2eb1cbb7c6dd7ec7c0587172ef28935f87292abd
Shreyas-ven <shreyasvbangera@gmail.com>
2026-04-25 12:01:18 +0530

    Interview question Pushed

--- DAY_25_04/p2_interviewBased.java
+++ DAY_25_04/p2_interviewBased.java
@@ -1,21 +1,25 @@
 public class p2_interviewBased {
-    public static void main(String args[]){
-        //int [] arr=new int[5];
-
-
-        //normal method 
-        int [] arr = {1,2,3,4,5};
-
-        int sum=0;
-        for (int i=1 ; i<=3; i++){
-            sum=sum+arr[i];
+public static void main(String[] args) {
+        int[] arr = {12,3,4,55,67,34,12,2,33,55};
+        int n = arr.length;
+        int[] prSum = new int[n];
+        prSum[0] = arr[0];
+        for(int i=1 ; i < arr.length; i ++){
+            prSum[i] = prSum[i-1]+arr[i];
         }
-        System.out.println("Sum : "+ sum);
+        for(int k: arr){
+            System.out.print(k + "  ");
+        }
+        System.out.print("\n");
+        for(int k: prSum){
+            System.out.print(k + "  ");
+        }
+        System.out.print("\n");
 
+        int L = 1,R=5;
 
-
-        //prefix sum
-
-
+        int prSum_LR = prSum[R] - prSum[L-1];
+        System.out.println("Sum of range between index L=1 to R=3 is: " + prSum_LR);
+        
     }
 }
