55f6e609e738a65d5504f660176322416b4a3268
Shreyas-ven <shreyasvbangera@gmail.com>
2026-04-29 16:21:20 +0530

    Afternoon push

---
+++ DAY_29_04/lms1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class lms1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        // Convert string to char array
+        char[] arr = input.toCharArray();
+
+        // Sort characters
+        Arrays.sort(arr);
+
+        // Print sorted string
+        System.out.println(new String(arr));
+    }
+}
